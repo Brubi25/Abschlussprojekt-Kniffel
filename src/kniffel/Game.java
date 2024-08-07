8ebbebb62baa1bb77a12b245ba95de46edfc5a17
Brubi25 <148341586+Brubi25@users.noreply.github.com>
2024-08-07 13:43:59 +0200

    zugriff auf game

--- src/kniffel/Game.java
+++ src/kniffel/Game.java
@@ -3,10 +3,52 @@ package kniffel;
 public class Game {
 	private Spieler[] spieler;
 	private int anzSpieler;
+	private int curSpieler;
+	private Wurf curWurf;
 	
 	Game(){
 		spieler = new Spieler[6];
 		anzSpieler = 0;
+	}
+	/**
+	 * Spielt Hand
+	 * @param hand
+	 * @return true falls erfolgreich, false sonst
+	 */
+	public boolean handSpielen(String hand) {
+		if(spieler[curSpieler].play(hand, this.curWurf)) {
+			nextPlayer();
+			return true;
+		}
+		return false;
+	}
+	
+	public int getWert(String hand) {
+		return spieler[curSpieler].getWert(hand, curWurd);
+	}
+	
+	public int nextPlayer() {
+		return curSpieler = (curSpieler + 1) % spieler.length;
+	}
+	
+	public int getCurSpieler() {
+		return this.curSpieler;
+	}
+	
+	public Wurf getCurWurf() {
+		return this.curWurf;
+	}
+	
+	public Wurf wurfeln(boolean[] wurfel) {
+		if(this.curWurf == null) {
+			this.wurfeln();
+			return this.curWurf;
+		}
+		return this.curWurf = Wurf.wuerfeln();
+	}
+	
+	public Wurf wurfeln() {
+		return this.curWurf = Wurf.wuerfeln();
 	}
 	
 	public void addPlayer(String name) {
@@ -28,6 +70,8 @@ public class Game {
 			spieler[pos] = spieler[pos+1];
 		}
 	}
+	
+	
 	
 	//eigentlich Nutzlos
 	public void printAll(Wurf W) {
